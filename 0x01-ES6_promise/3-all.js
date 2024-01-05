39961354fa36dde5dbd337c3b77e1ad1c2d64c0b
Prosper Mugisha <mprosp3r@gmail.com>
2024-01-05 04:57:15 +0200

    fix task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-async function handleProfileSignup() {
-  try {
-    const res = await Promise.all([createUser(), uploadPhoto()]);
-    console.log(`${res[1].body} ${res[0].firstName} ${res[0].lastName} `);
-  } catch (error) {
-    console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  return Promise.all([createUser(), uploadPhoto()])
+    .then((res) => {
+      console.log(`${res[1].body} ${res[0].firstName} ${res[0].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
-export default handleProfileSignup;
